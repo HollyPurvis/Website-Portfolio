e32868a5051a3bca380e34abf714117c4e38d2a5
HollyPurvis <hmp21@students.uwf.edu>
2023-04-29 12:29:27 -0500

    Added and Setup Hero component

---
+++ my-portfolio/src/components/Hero.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <div class="row">
+                <div class="col-lg-8 col-md-8 col-12">
+                    <p>Hello, I'm Holly Purvis a</p>
+                    <h1>Front End Web Developer</h1>
+                    <p>
+                        A Web Developer with a strong background in web support and maintenance, 
+                        I am passionate about creating dynamic and innovative web solutions that enhance user experiences. 
+                        With expertise in web development and a keen eye for detail, I am committed to producing clean, efficient, 
+                        and maintainable code while adhering to industry standards and best practices. I am highly motivated, adaptable, 
+                        and always seeking opportunities to enhance my skills and knowledge.
+                    </p>
+                    <p>Section </p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    photo
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import Portfolio from './components/Portfolio';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      Portfolio
-    </div>
+    <Portfolio />
   </React.StrictMode>
 );
 
